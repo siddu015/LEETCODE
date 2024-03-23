bad7f07f54106801e972c8697803ec521ec86976
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-24 00:36:06 +0530

    Done

--- ACE 75/src/EqualRowAndColumnPairs.java
+++ ACE 75/src/EqualRowAndColumnPairs.java
@@ -2,25 +2,21 @@ import java.util.HashMap;
 
 public class EqualRowAndColumnPairs {
     public int equalPairs(int[][] grid) {
-        int n = grid.length, result = 0;
+        int len = grid.length, count = 0;
 
-        HashMap<Long, Integer> rows= new HashMap<>();
-
-        for (int[] values : grid) {
-            long t1 = 0;
-            for (int val: values)
-                t1 = t1 * 10 + val;
-            rows.put(t1, rows.getOrDefault(t1, 0) + 1);
+        for (int[] ints : grid) {
+            for (int j = 0; j < len; j++) {
+                boolean isEqual = true;
+                for (int k = 0; k < len; k++) {
+                    if (ints[k] != grid[k][j]) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+                if (isEqual) count++;
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            long t2 = 0;
-            for (int[] ints : grid) t2 = t2 * 10 + ints[i];
-
-            result += rows.getOrDefault(t2, 0);
-        }
-
-        return result;
-
+        return count;
     }
 }
