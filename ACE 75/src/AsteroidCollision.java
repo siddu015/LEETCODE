8440bce76362385e637c0f88eef45c04bea6c29d
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-28 23:15:29 +0530

    Done

---
+++ ACE 75/src/AsteroidCollision.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> result = new ArrayList<>();
+
+        int temp = 0;
+        for(int i = 1; i < asteroids.length; i++) {
+            if(asteroids[i] > 0) {
+                if(asteroids[i++] > 0) {
+
+                }
+            }
+        }
+    }
+}
--- ACE 75/src/Main.java
+++ ACE 75/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-       var ex = new RemovingStars();
-        String s = "leet**cod*e";
-        System.out.println(ex.removeStars(s));
+
 
     }
 }
