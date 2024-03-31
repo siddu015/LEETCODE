3dd720a388ea776b678cdc5177ecf0469bb74e67
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-31 21:59:19 +0530

    Done

--- ACE 75/src/AsteroidCollision.java
+++ ACE 75/src/AsteroidCollision.java
@@ -1,17 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class AsteroidCollision {
     public int[] asteroidCollision(int[] asteroids) {
-        List<Integer> result = new ArrayList<>();
+        int n = asteroids.length;
+        int j = 0;
 
-        int temp = 0;
-        for(int i = 1; i < asteroids.length; i++) {
-            if(asteroids[i] > 0) {
-                if(asteroids[i++] > 0) {
-
-                }
+        for (int i = 0; i < n; i++) {
+            int asteroid = asteroids[i];
+            while (j>0 && asteroids[j-1]>0 && asteroid<0 && asteroids[j-1] < Math.abs(asteroid)) {
+                j--;
             }
+
+            if (j==0 || asteroid>0 || asteroids[j-1]<0)
+                asteroids[j++] = asteroid;
+            else if(asteroids[j-1] == Math.abs(asteroid))
+                j--;
         }
+
+        int[] result = new int[j];
+        System.arraycopy(asteroids, 0, result, 0, j);
+
+        return result;
     }
 }
--- ACE 75/src/Main.java
+++ ACE 75/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        var ass = new AsteroidCollision();
+        int[] arr =  {10,2, -5};
+        var result = ass.asteroidCollision(arr);
 
+        System.out.println(Arrays.toString(result));
 
     }
 }
