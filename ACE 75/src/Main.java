67845a5467d5ba39650629e9d208323606e596fa
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-04-17 14:56:34 +0530

    Done

--- ACE 75/src/Main.java
+++ ACE 75/src/Main.java
@@ -3,11 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        var ass = new AsteroidCollision();
-        int[] arr =  {10,2, -5};
-        var result = ass.asteroidCollision(arr);
+        var ass = new DecodeString();
+        System.out.println(ass.decodeString("2[abc]3[cd]ef"));
 
-        System.out.println(Arrays.toString(result));
 
     }
 }
