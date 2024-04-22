d1ddc3ba2b4916ad540a8031ac54e1e2b8cd1dea
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-04-22 22:48:34 +0530

    Done

--- ACE 75/src/Main.java
+++ ACE 75/src/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         var ass = new KeysRooms();
-        int[][] arr = {{1,3},{3,0,1},{2},{0}};
-        var result = ass.canVisitAllRooms(arr, 4);
-        System.out.println(result);
 
     }
 }
