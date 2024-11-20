4a1e1aee113d6deb3a2acbdf144eab47cf699040
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-11-20 20:18:17 +0530

    Refactored 'Two Sum' problem solution for better performance

--- Top Interview 150/src/Main.java
+++ Top Interview 150/src/Main.java
@@ -5,82 +5,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = {3,2,3};
+        int[] nums = {2147483647,2147483647,2147483647,2147483647,2147483647,2147483647};
 
-        System.out.println(majorityElement(nums));
+        lengthOfLongestSubstring("au");
     }
 
-    public static void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        boolean firstRowHasZero = false;
-        boolean firstColHasZero = false;
+    public static int lengthOfLongestSubstring(String s) {
+        if(s.isEmpty())
+            return 0;
+        if(s.length() == 1)
+            return 1;
 
-        for (int i = 0; i < r; i++) {
-            if (matrix[i][0] == 0) {
-                firstRowHasZero = true;
-                break;
-            }
-        }
-        for (int i = 0; i < c; i++) {
-            if (matrix[0][i] == 0) {
-                firstColHasZero = true;
-                break;
-            }
-        }
 
-        for (int i = 1; i < r; i++) {
-            for (int j = 1; j < c; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
+        Set<Character> set = new HashSet<>();
+        int count = 0, result = 0;
+
+        for(var c: s.toCharArray()) {
+            if(set.contains(c)) {
+                result = Math.max(result, count);
+                count = 1;
+                set.clear();
+                set.add(c);
+            } else {
+                set.add(c);
+                count++;
             }
         }
 
-        for (int i = 1; i < r; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < c; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        return Math.max(result, count);
 
-        for (int j = 1; j < c; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < r; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        if (firstRowHasZero) {
-            for (int j = 0; j < c; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (firstColHasZero) {
-            for (int i = 0; i < r; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-    }
-
-    public static int majorityElement(int[] nums) {
-        Hashtable<Integer, Integer> dict= new Hashtable<>();
-        int max = 0, result = 0;
-        for (int num : nums) {
-            if (dict.containsKey(num))
-                dict.put(num, dict.get(num) + 1);
-            else
-                dict.put(num, 1);
-
-            if(dict.get(num) > max) {
-                max = dict.get(num);
-                result = num;
-            }
-        }
-
-        return result;
     }
 }
